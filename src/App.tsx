2eea8c74cd452eae786b77f858542fe7e2f19f64
EnzoK <kropotin_aleksey@mail.ru>
2022-10-18 11:06:17 +0300

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {DragEvent, useState} from 'react';
+import { v1 } from 'uuid';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [boards, setBoards] = useState([
+    {
+      id: v1(), title: 'Users', items: [{id: v1(), order: 1, name: 'Bob'},
+        {id: v1(), order: 2, name: 'Mike'},
+        {id: v1(), order: 3, name: 'Frank'},]
+    },
+    {
+      id: v1(), title: 'Mentors', items: [{id: v1(), order: 1, name: 'Bobby'},
+        {id: v1(), order: 2, name: 'Mikes'},
+        {id: v1(), order: 3, name: 'Franks'},]
+    }
+
+  ])
+  const [currentBoard, setCurrentBoards] = useState<any>()
+  const [currentItem, setCurrentItem] = useState<any>()
+  const onDragOverHandle = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+  }
+  const onDragLeaveHandle = (e: DragEvent<HTMLDivElement>) => {
+  }
+  const onDragStartHandle = (e: DragEvent<HTMLDivElement>, board: any, item: any) => {
+    setCurrentBoards(board)
+    setCurrentItem(item)
+  }
+  const onDragEndHandle = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+  }
+  const onDropHandle = (e: DragEvent<HTMLDivElement>, board: any, item: any) => {
+    e.preventDefault()
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    currentBoard.items.splice(currentIndex, 1)
+    const dropIndex = board.items.indexOf(item)
+    board.items.splice(dropIndex + 1, 0, currentItem)
+    setBoards(boards.map((b: any) => {
+      if (b.id === board.id) {
+        return board
+      }
+      if (b.id === currentBoard.id) {
+        return currentBoard
+      }
+      return b
+    }))
+  }
+
+  const onCardHandle = (e: DragEvent<HTMLDivElement>, board: any) => {
+    board.items.push(currentItem)
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    currentBoard.items.splice(currentIndex, 1)
+    setBoards(boards.map((b: any) => {
+      if (b.id === board.id) {
+        return board
+      }
+      if (b.id === currentBoard.id) {
+        return currentBoard
+      }
+      return b
+    }))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'wrapper'}>
+      {boards.map((board =>
+          <div key={board.id} className={'board__wrapper'}
+               onDragOver={(e) => onDragOverHandle(e)}
+               onDrop={(e) => onCardHandle(e, board)}>
+            <h4 className={'board__title'}>{board.title}</h4>
+            {board.items.map((item =>
+                <div draggable
+                     onDragOver={(e) => onDragOverHandle(e)}
+                     onDragLeave={(e) => onDragLeaveHandle(e)}
+                     onDragStart={(e) => onDragStartHandle(e, board, item)}
+                     onDragEnd={(e) => onDragEndHandle(e)}
+                     onDrop={(e) => onDropHandle(e, board, item)}
+                     key={item.id} className={'board__item'}>{item.name}</div>
+            ))}
+          </div>
+      ))}
     </div>
   );
 }
